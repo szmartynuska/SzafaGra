557d73f0d7443f8aff78466082fd2b6b0fa59af8
maugosch <mk34694@gmail.com>
2018-04-25 15:29:57 +0200

    Added buttons, minor changes

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,21 @@
-$('.navbar').click(function(event) {
-  $('.nav').toggleClass('active');
-});
+$(document).ready(function(){
+
+    $('.navbar').click(function(event) {
+        $('.nav').toggleClass('active');
+    });
+
+    /*
+    function buttons() {
+        var x = document.getElementById("buttons");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    } */
+
+        $(".btn-slide").click(function(){
+            $("#buttons").slideToggle();
+        });
+
+    });
