378fa5bba82770d191ee8d9751b3923b9860b5f8
maugosch <mk34694@gmail.com>
2018-04-26 14:29:39 +0200

    Dynamic wardrobe addition

--- www/js/index.js
+++ www/js/index.js
@@ -17,5 +17,32 @@ $(document).ready(function(){
         $(".btn-slide").click(function(){
             $("#buttons").slideToggle();
         });
+    
+    var img_index = 1;
+
+    $('.btn-war').click(function() {
+        var img = $('<img />').attr({
+            'id': 'myImage'+img_index,
+            'src': 'img/wardrobe.svg',
+            'class': 'myWardrobe',
+            'width': 96.55,
+            'height': 100,
+            'href': '#wardrobe-cats'
+        }).appendTo('.menu-wardrobe');
+        
+    $("#myImage"+img_index).wrap($('<a>',{
+           href: '#wardrobe-cats'
+            }));
+        
+        
+        var someText = "Wardrobe "+img_index;
+        $('.menu-wardrobe').append(someText);
+        
+        img_index++;
+        
+    });
+    
+    $('#war-nr').append(img_index);
+    
 
     });
